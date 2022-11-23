e17159c3255c9c3a6e901a06dc8a94418c64741d
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 17:24:39 -0600

    feat(layout): add syles to footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>Handcrafted with 💜 and t3-stack by codingcodax</p>
+    <footer className='mx-auto w-full max-w-6xl'>
+      <p className='py-4 text-center'>
+        Handcrafted with 💜 and t3-stack by codingcodax
+      </p>
     </footer>
   );
 };
