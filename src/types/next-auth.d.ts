7677018b25777054219602bed30e51b8f6dadfe1
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 13:54:33 -0600

    feat(types): add username to session interface and create user interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,6 +7,11 @@ declare module 'next-auth' {
   interface Session {
     user?: {
       id: string;
+      username?: string | undefined;
     } & DefaultSession['user'];
+  }
+
+  interface User {
+    username?: string | undefined;
   }
 }
