a79dfef82bd3ec084584f8473d77e248efed9c8e
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 16:59:59 -0600

    feat(header): add styles

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -12,30 +12,48 @@ const links = [
   },
 ];
 
+const menuItemStyles =
+  'block w-full rounded px-3 py-2 text-left text-sm text-mauve-1100 transition-colors duration-200 hover:bg-mauve-400 dark:hover:bg-mauveDark-400';
+
 const Header = () => {
   const { data: session, status } = useSession();
 
   return (
-    <header>
-      <Link href='/'>Slug</Link>
+    <header className='mx-auto flex w-full max-w-6xl items-center justify-between py-4'>
+      <Link className='text-2xl' href='/'>
+        slug
+      </Link>
 
       {status === 'unauthenticated' ? (
         <Link href='login'>Sign in</Link>
       ) : (
-        <Menu>
-          <Menu.Button>{session?.user?.name}</Menu.Button>
-          <Menu.Items>
+        <Menu as='div' className='relative inline-block'>
+          <Menu.Button className='rounded-md px-3 py-2 hover:bg-mauve-400 focus:outline-none focus-visible:bg-mauve-200 focus-visible:ring-1 focus-visible:ring-mauve-700 focus-visible:ring-offset-2 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-200 dark:focus-visible:ring-mauveDark-700'>
+            {session?.user?.username} &darr;
+          </Menu.Button>
+          <Menu.Items className='absolute right-0 z-40 mt-2 w-56 origin-top-right rounded-md border border-mauve-600 bg-mauve-200 p-1 shadow-lg focus:outline-none dark:divide-gray-600 dark:border-mauveDark-600 dark:bg-mauveDark-200'>
             {links.map(({ href, label, isExternal }) => (
               <Menu.Item key={href}>
                 {isExternal ? (
-                  <a href={href} rel='noopener noreferrer' target='_blank'>
+                  <a
+                    className={menuItemStyles}
+                    href={href}
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
                     {label}
                   </a>
                 ) : (
-                  <Link href={href}>{label}</Link>
+                  <Link className={menuItemStyles} href={href}>
+                    {label}
+                  </Link>
                 )}
               </Menu.Item>
             ))}
+
+            <Menu.Item>
+              <button className={menuItemStyles}>Sign out</button>
+            </Menu.Item>
           </Menu.Items>
         </Menu>
       )}
