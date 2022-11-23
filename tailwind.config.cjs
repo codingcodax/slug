394000222f7b139cccf84619a4f7978f93f2cb1a
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 11:53:34 -0600

    chore(tailwind): update font sans

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: { sans: ['var(--font-inter)', ...fontFamily.sans] },
       colors: {
         mauve: {
           100: '#fdfcfd',
