dd49ded4186c28ec99c1959e9c484e125994ab78
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 11:53:47 -0600

    feat(app): add inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import { type AppProps } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
+import { Inter } from '@next/font/google';
 
 import { trpc } from '~/utils/trpc';
 import '~/styles/globals.css';
 import Layout from '~/components/layout';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 const MyApp = ({
   Component,
@@ -14,9 +20,11 @@ const MyApp = ({
   return (
     <ThemeProvider attribute='class'>
       <SessionProvider session={session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <div className={`${inter.variable} font-sans`}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
       </SessionProvider>
     </ThemeProvider>
   );
