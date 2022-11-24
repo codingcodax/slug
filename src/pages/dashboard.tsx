78c391d9aff96b1bccf2d40c5e3a147683548c75
Alexis Guzman <alexis12989x@gmail.com>
2022-11-24 11:37:28 -0600

    feat(dashboard): add tittle and create link button

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { type GetServerSideProps } from 'next';
+import Link from 'next/link';
 
+import { Icons } from '~/components/ui';
 import { getServerAuthSession } from '~/server/common/get-server-auth-session';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
@@ -15,9 +17,27 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 const Dashboard = () => {
   return (
-    <div>
-      <p>Dashboard page</p>
-    </div>
+    <main className='mx-auto w-full max-w-6xl'>
+      <div className='mb-10 flex items-center justify-between'>
+        <h1 className='text-3xl font-medium'>Dashboard</h1>
+        <Link
+          className='flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 dark:hover:bg-mauveDark-400'
+          href='/dashboard/new'
+        >
+          <Icons.Plus className='mr-2 h-4 w-4' /> Create new link
+        </Link>
+      </div>
+
+      <div className='flex flex-col items-center'>
+        <p>You don&apos;t have any links.</p>
+        <Link
+          className='flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 dark:hover:bg-mauveDark-400'
+          href='/dashboard/new'
+        >
+          <Icons.Plus className='mr-2 h-4 w-4' /> Create one first
+        </Link>
+      </div>
+    </main>
   );
 };
 
