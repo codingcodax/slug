505e303e17d3388f167415094f9ebdbe5f18118a
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 21:26:53 -0600

    feat(dashboard): add redirect when session dont exist with get server auth session

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,18 @@
+import { type GetServerSideProps } from 'next';
+
+import { getServerAuthSession } from '~/server/common/get-server-auth-session';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session)
+    return {
+      redirect: { destination: '/sign-in', permanent: false },
+    };
+
+  return { props: { session } };
+};
+
 const Dashboard = () => {
   return (
     <div>
