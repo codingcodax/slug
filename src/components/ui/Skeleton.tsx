40a2e30ce25b2aa26a6228125251e3a7d059ee87
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 20:02:53 -0600

    feat(ui): setup skeleton

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -1,8 +1,16 @@
-const Skeleton = () => {
+import cn from '~/utils/cn';
+
+type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
+
+const Skeleton = ({ className, ...props }: SkeletonProps) => {
   return (
-    <div>
-      <p>Sekeleton component</p>
-    </div>
+    <div
+      className={cn(
+        'h-4 w-2/5 animate-pulse rounded-md bg-mauve-400 dark:bg-mauveDark-400',
+        className
+      )}
+      {...props}
+    />
   );
 };
 
