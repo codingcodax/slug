fc9f73fc79019f06ab9e40bb1debabb48638afc4
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 19:31:17 -0600

    feat(home): move text center from main to div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <main className='mx-auto mt-10 flex max-w-6xl flex-col items-center space-y-10 text-center'>
-      <div className='space-y-4'>
+    <main className='mx-auto mt-10 flex max-w-6xl flex-col items-center space-y-10'>
+      <div className='space-y-4 text-center'>
         <p className='text-sm uppercase'>Meet slug</p>
         <h1 className='text-md text-6xl font-medium capitalize'>
           Open source link shortener
