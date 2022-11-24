66b543fc701b0cc5180147d104c10b267a757b7a
Alexis Guzman <alexis12989x@gmail.com>
2022-11-23 21:12:22 -0600

    feat(footer): add external links

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,25 @@ const Footer = () => {
   return (
     <footer className='mx-auto w-full max-w-6xl'>
       <p className='py-4 text-center'>
-        Handcrafted with 💜 and t3-stack by codingcodax
+        Handcrafted with <span className='font-mono'>🤍</span> and{' '}
+        <a
+          className='font-medium underline decoration-wavy underline-offset-2'
+          href='https://create.t3.gg/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          t3-stack
+        </a>{' '}
+        by{' '}
+        <a
+          className='font-medium underline decoration-wavy underline-offset-2'
+          href='https://www.codingcodax.dev/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          &#64;codingcodax
+        </a>{' '}
+        ^_^
       </p>
     </footer>
   );
