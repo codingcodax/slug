9a8b7000fc35ec5e8e5c67761ecff6a48f346639
Alexis Guzman <alexis12989x@gmail.com>
2022-11-24 21:28:02 -0600

    feat(middleware): add new to excluded paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,5 @@ export const middleware = async (req: NextRequest) => {
 };
 
 export const config = {
-  matcher: ['/((?!api|_next/static|favicon.ico|dashboard|sign-in).*)'],
+  matcher: ['/((?!api|_next/static|favicon.ico|dashboard|sign-in|new).*)'],
 };
