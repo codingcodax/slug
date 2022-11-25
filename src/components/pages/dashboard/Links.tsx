87f79ba251f01c57dfd01a9e47e3f3ca66623b6b
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 16:57:21 -0600

    feat(dashboard): create links component

--- src/components/pages/dashboard/Links.tsx
+++ src/components/pages/dashboard/Links.tsx
@@ -1,9 +1,32 @@
-const Links = () => {
+import { type LinkSchema } from '~/server/trpc/router/linkRouter';
+
+import LinkItem from './LinkItem';
+
+interface LinksProps {
+  links: LinkSchema[] | undefined;
+}
+
+const Links = ({ links }: LinksProps) => {
   return (
-    <div>
-      <p>Links Component</p>
-    </div>
+    <ul className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+      {links?.map(({ id, slug, url, description }) => (
+        <LinkItem key={id}>
+          <LinkItem.Slug slug={slug}>{slug}</LinkItem.Slug>
+          <LinkItem.Link>{url}</LinkItem.Link>
+          <LinkItem.Description>{description}</LinkItem.Description>
+        </LinkItem>
+      ))}
+    </ul>
   );
 };
+
+// eslint-disable-next-line react/display-name
+Links.Skeleton = () => (
+  <ul className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+    <LinkItem.Skeleton />
+    <LinkItem.Skeleton />
+    <LinkItem.Skeleton />
+  </ul>
+);
 
 export default Links;
