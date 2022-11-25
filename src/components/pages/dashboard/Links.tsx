17856c317302a2267b45316b80331029d08adfd1
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 17:49:03 -0600

    feat(dashboard): use link item variants

--- src/components/pages/dashboard/Links.tsx
+++ src/components/pages/dashboard/Links.tsx
@@ -24,8 +24,8 @@ const Links = ({ links }: LinksProps) => {
 Links.Skeleton = () => (
   <ul className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
     <LinkItem.Skeleton />
-    <LinkItem.Skeleton />
-    <LinkItem.Skeleton />
+    <LinkItem.Skeleton variant={2} />
+    <LinkItem.Skeleton variant={1} />
   </ul>
 );
 
