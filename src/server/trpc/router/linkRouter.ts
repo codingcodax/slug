cdf0405274ada439f7297ec67b69e3d28168d8ab
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 16:46:06 -0600

    feat(trpc): create link schema for link router

--- src/server/trpc/router/linkRouter.ts
+++ src/server/trpc/router/linkRouter.ts
@@ -2,12 +2,22 @@ import { z } from 'zod';
 
 import { router, protectedProcedure } from '~/server/trpc/trpc';
 
+export const LinkSchema = z.object({
+  id: z.number(),
+  url: z.string(),
+  slug: z.string(),
+  createdAt: z.date(),
+  creatorId: z.string(),
+  description: z.string().nullish(),
+});
+
 export const CreateSchema = z.object({
   url: z.string(),
   slug: z.string(),
   description: z.string().nullish(),
 });
 
+export type LinkSchema = z.infer<typeof LinkSchema>;
 export type CreateLink = z.infer<typeof CreateSchema>;
 
 export const linkRouter = router({
