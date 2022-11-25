a349ed460779354367a708cb8231b0b11ef03886
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 17:53:11 -0600

    refactor(home): use hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,12 @@
 import { type NextPage } from 'next';
-import Link from 'next/link';
+
+import { Hero } from '~/components/pages/home';
 
 const Home: NextPage = () => {
   return (
-    <main className='mx-auto mt-10 flex max-w-6xl flex-col items-center space-y-10'>
-      <div className='space-y-4 text-center'>
-        <p className='text-sm uppercase'>Meet slug</p>
-        <h1 className='text-md text-6xl font-medium capitalize'>
-          Open source link shortener
-        </h1>
-        <p className='text-2xl text-mauve-1100 dark:text-mauveDark-1100'>
-          slug is a link managment plarform to shorten your links for devs.
-        </p>
-      </div>
-
-      <div className='space-x-4'>
-        <Link className='primary-link' href='/dashboard'>
-          Get Started &rarr;
-        </Link>
-
-        <a
-          className='secondary-link'
-          href='https://github.com/codingcodax/slug'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          Star on GitHub
-        </a>
-      </div>
-    </main>
+    <div>
+      <Hero />
+    </div>
   );
 };
 
