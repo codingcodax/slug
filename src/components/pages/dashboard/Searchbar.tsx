e3e1ab9c00fe949033850f11fcac8c50fb13cbb3
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 17:12:48 -0600

    feat(dashboard): setup searchbar component

--- src/components/pages/dashboard/Searchbar.tsx
+++ src/components/pages/dashboard/Searchbar.tsx
@@ -1,7 +1,31 @@
-const Searchbar = () => {
+import type { ChangeEvent } from 'react';
+
+import { Icons } from '~/components/ui';
+import cn from '~/utils/cn';
+
+interface SearchbarProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  isDisabled: boolean;
+}
+
+const Searchbar = ({ value, onChange, isDisabled }: SearchbarProps) => {
   return (
-    <div>
-      <p>Searchbar Component</p>
+    <div className='relative mb-4 w-full'>
+      <div className='absolute top-0 left-0 flex h-[42px] w-10 items-center justify-center'>
+        <Icons.Search className='h-6 w-6 stroke-mauve-1100 dark:stroke-mauveDark-1000' />
+      </div>
+      <input
+        className={cn(
+          'input w-full pl-10',
+          isDisabled ? 'cursor-not-allowed' : ''
+        )}
+        disabled={isDisabled}
+        placeholder='Search'
+        type='text'
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
