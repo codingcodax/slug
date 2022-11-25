37263ba99855d3fd262fe64a022c66492b9b0279
Alexis Guzman <alexis12989x@gmail.com>
2022-11-24 22:10:53 -0600

    feat(pages): setup new

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,8 +1,132 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { toast } from 'react-hot-toast';
+
+import { trpc } from '~/utils/trpc';
+import cn from '~/utils/cn';
+import { Icons } from '~/components/ui';
+
 const New = () => {
+  const router = useRouter();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { mutate: createLink } = trpc.link.create.useMutation({
+    onSuccess: () => {
+      router.push('/dashboard');
+      setIsLoading(false);
+    },
+    onError: (data) => {
+      setIsLoading(false);
+      console.log(data);
+    },
+  });
+
+  const onSubmit = (values) => {
+    setIsLoading(true);
+    createLink(values);
+  };
+
   return (
-    <div>
-      <p>New</p>
-    </div>
+    <main className='mx-auto w-full max-w-xl'>
+      <h1 className='mb-10 text-3xl font-medium'>New</h1>
+
+      <form className='grid max-w-xl gap-y-8' onSubmit={handleSubmit(onSubmit)}>
+        <div className='space-y-4'>
+          <div className='grid grid-flow-row gap-y-1'>
+            <label htmlFor='url'>Enter your link here:</label>
+            <input
+              className='input'
+              id='url'
+              placeholder='https://github.com/codingcodax/'
+              type='text'
+              {...register('url', {
+                required: { value: true, message: 'Please enter a URL' },
+                minLength: {
+                  value: 10,
+                  message:
+                    'Please enter a valid URL. It should be at least 10 characters long',
+                },
+                pattern: {
+                  value: /^https?:\/\//i,
+                  message:
+                    'Please enter a valid URL. It should start with http:// or https://',
+                },
+              })}
+            />
+
+            {errors.url && <Error>{errors.url.message?.toString()}</Error>}
+          </div>
+
+          <div className='grid grid-flow-row gap-y-1'>
+            <label htmlFor='slug'>
+              Select your slug <br />
+              <span className='text-sm text-mauve-1100 dark:text-mauveDark-1100'>
+                https://slug.codingcodax.dev/&#36;&#10100;your-slug&#10101;
+              </span>
+            </label>
+            <input
+              className='input'
+              id='slug'
+              placeholder='my-portfolio'
+              type='text'
+              {...register('slug', {
+                required: {
+                  value: true,
+                  message:
+                    'Please enter a custom slug or generate a random one',
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9_-]+$/i,
+                  message:
+                    'Please enter a valid slug with blank space or special characters.',
+                },
+              })}
+            />
+
+            {errors.slug && <Error>{errors.slug.message?.toString()}</Error>}
+          </div>
+
+          <div className='grid grid-flow-row gap-y-1'>
+            <label htmlFor='Description'>Description (optional)</label>
+            <textarea
+              className={cn('input max-h-32')}
+              id='description'
+              placeholder='This link redirecs to my awesome portfolio'
+              {...register('description')}
+            />
+          </div>
+        </div>
+
+        <div className='grid grid-cols-2 gap-x-4'>
+          <button className='primary-button' type='submit'>
+            {isLoading ? 'Shorten your link...' : 'Short your link'}
+          </button>
+          <Link className='secondary-link' href='/dashboard'>
+            Cancel
+          </Link>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+interface ErrorProps {
+  children: React.ReactNode;
+}
+
+const Error = ({ children }: ErrorProps) => {
+  return (
+    <span className='flex items-center text-sm text-red-900'>
+      <Icons.AlertCircle className='mr-2 h-4 w-4 stroke-red-900' />
+      {children}
+    </span>
   );
 };
 
