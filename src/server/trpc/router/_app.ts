4e05037a0e2dafffe529be375acd39c1d9fd1e95
Alexis Guzman <alexis12989x@gmail.com>
2022-11-24 21:29:44 -0600

    feat(trpc): add link router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,10 +2,12 @@ import { router } from '~/server/trpc/trpc';
 
 import { exampleRouter } from './example';
 import { authRouter } from './auth';
+import { linkRouter } from './linkRouter';
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  link: linkRouter,
 });
 
 // export type definition of API
