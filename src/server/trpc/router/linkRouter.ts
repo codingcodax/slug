607e2448f0e14e948e790f463d5f574481799313
Alexis Guzman <alexis12989x@gmail.com>
2022-11-24 21:29:33 -0600

    feat(trpc): create link router

---
+++ src/server/trpc/router/linkRouter.ts
@@ -0,0 +1,50 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
+import { router, protectedProcedure } from '~/server/trpc/trpc';
+
+export const linkRouter = router({
+  create: protectedProcedure
+    .input(
+      z.object({
+        url: z.string(),
+        slug: z.string(),
+        description: z.string().nullish(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const linkExists = await ctx.prisma.link.findFirst({
+        where: { slug: { equals: input.slug } },
+      });
+
+      console.log('link exists??', linkExists);
+
+      if (linkExists !== null)
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message:
+            'Custom slug is not available. Try another one or generate randomly.',
+        });
+
+      const newLink = ctx.prisma.link.create({
+        data: {
+          ...input,
+          creatorId: ctx.session.user.id,
+        },
+      });
+
+      return newLink;
+    }),
+  get: protectedProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.link.findFirst({
+        where: { slug: { equals: input.slug } },
+      });
+    }),
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.link.findMany({
+      where: { creatorId: ctx.session.user.id },
+    });
+  }),
+});
