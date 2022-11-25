7855113287eaa1babfc9768ffbe075e24cd84cb7
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 16:57:58 -0600

    refactor(dashboard): use links components

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { type GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { useState } from 'react';
 
+import { Links } from '~/components/pages/dashboard';
 import { Icons, Skeleton } from '~/components/ui';
 import { getServerAuthSession } from '~/server/common/get-server-auth-session';
 import cn from '~/utils/cn';
@@ -55,13 +56,8 @@ const Dashboard = () => {
         />
       </div>
 
-      {isLoading && (
-        <ul className='space-y-4'>
-          <LinkItem.Skeleton />
-          <LinkItem.Skeleton />
-          <LinkItem.Skeleton />
-        </ul>
-      )}
+      {isLoading && <Links.Skeleton />}
+      {links && <Links links={filteredLinks} />}
 
       {links?.length === 0 && (
         <section className='flex flex-col items-center'>
@@ -74,74 +70,7 @@ const Dashboard = () => {
           </Link>
         </section>
       )}
-
-      {links && (
-        <ul className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-          {filteredLinks?.map(({ id, slug, url, description }) => (
-            <LinkItem key={id}>
-              <LinkItem.Slug slug={slug}>{slug}</LinkItem.Slug>
-              <LinkItem.Link>{url}</LinkItem.Link>
-              <LinkItem.Description>{description}</LinkItem.Description>
-            </LinkItem>
-          ))}
-        </ul>
-      )}
     </main>
-  );
-};
-
-interface LinkItemProps {
-  children: React.ReactNode;
-}
-
-const LinkItem = ({ children }: LinkItemProps) => {
-  return (
-    <li className='rounded-md border border-mauve-600 p-4 transition-colors duration-200 hover:border-mauve-700 dark:border-mauveDark-600 dark:hover:border-mauveDark-700'>
-      {children}
-    </li>
-  );
-};
-
-interface LinkItemSlugProps {
-  children: React.ReactNode;
-  slug: string;
-}
-
-// eslint-disable-next-line react/display-name
-LinkItem.Slug = ({ children, slug }: LinkItemSlugProps) => {
-  return (
-    <Link className='text-xl' href={`https://slug.codingcodax.dev/${slug}`}>
-      /{children}
-    </Link>
-  );
-};
-
-interface LinkItemLinkProps {
-  children: React.ReactNode;
-}
-
-// eslint-disable-next-line react/display-name
-LinkItem.Link = ({ children }: LinkItemLinkProps) => {
-  return <p className='text-mauve-1100 dark:text-mauveDark-1100'>{children}</p>;
-};
-
-interface LinkItemDescriptionProps {
-  children: React.ReactNode;
-}
-
-// eslint-disable-next-line react/display-name
-LinkItem.Description = ({ children }: LinkItemDescriptionProps) => {
-  return <p className='mt-2'>{children || 'No description'}</p>;
-};
-
-// eslint-disable-next-line react/display-name
-LinkItem.Skeleton = () => {
-  return (
-    <li className='rounded-md border border-mauve-600 p-4 dark:border-mauveDark-600'>
-      <Skeleton className='h-6 bg-mauve-500 dark:bg-mauveDark-500' />
-      <Skeleton className='mt-2 h-5 w-3/5 bg-mauve-300 dark:bg-mauveDark-300' />
-      <Skeleton className='mt-3 h-5 w-full' />
-    </li>
   );
 };
 
