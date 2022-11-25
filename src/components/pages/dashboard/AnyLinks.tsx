bb95ba6b82ab3c40b2071f4cbdd53599e13935f5
Alexis Guzman <alexis12989x@gmail.com>
2022-11-25 17:01:28 -0600

    feat(dashboard): setup any links component

--- src/components/pages/dashboard/AnyLinks.tsx
+++ src/components/pages/dashboard/AnyLinks.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+
+import { Icons } from '~/components/ui';
+
 const AnyLinks = () => {
   return (
-    <div>
-      <p>Any Links Component</p>
-    </div>
+    <section className='flex flex-col items-center'>
+      <p>You don&apos;t have any links.</p>
+      <Link
+        className='flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 dark:hover:bg-mauveDark-400'
+        href='/dashboard/new'
+      >
+        <Icons.Plus className='mr-2 h-4 w-4' /> Create one first
+      </Link>
+    </section>
   );
 };
 
