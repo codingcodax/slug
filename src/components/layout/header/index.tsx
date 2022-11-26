ab2c711d3ead3ff5171abc4d7a926203ea109f19
Alexis Guzman <alexis12989x@gmail.com>
2022-11-26 16:58:47 -0600

    refactor(header): use logo, sign in link and auth dropdown

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,32 +1,8 @@
-import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
-
-import { Dropdown, Icons, Skeleton } from '~/components/ui';
+import { useSession } from 'next-auth/react';
 
 import Logo from './Logo';
 import SignInLink from './SignInLink';
-
-const links = [
-  {
-    href: '/dashboard/new',
-    label: 'Create new link',
-    icon: <Icons.Plus className='mr-2 h-4 w-4' />,
-  },
-  {
-    href: '/dashboard',
-    label: 'Dashboard',
-    icon: <Icons.LayoutList className='mr-2 h-4 w-4' />,
-  },
-  {
-    href: 'https://github.com/codingcodax/slug/issues',
-    label: 'Report a bug',
-    icon: <Icons.CircleDot className='mr-2 h-4 w-4' />,
-    isExternal: true,
-  },
-];
-
-const menuItemStyles =
-  'flex flex-row items-center block w-full rounded px-3 py-2 text-left text-sm text-mauve-1100 dark:text-mauveDark-1100 transition-colors duration-200 hover:bg-mauve-400 dark:hover:bg-mauveDark-400 ui-active:bg-mauve-400 dark:ui-active:bg-mauveDark-400';
+import AuthDropdown from './AuthDropdown';
 
 const Header = () => {
   const { data: session, status } = useSession();
@@ -38,50 +14,10 @@ const Header = () => {
       {status === 'unauthenticated' ? (
         <SignInLink />
       ) : (
-        <Dropdown>
-          <Dropdown.Title className='flex items-center rounded-md px-3 py-2 hover:bg-mauve-400 focus-visible:bg-mauve-400 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400'>
-            {status === 'loading' ? (
-              <Skeleton className='h-6 w-[70px]' />
-            ) : (
-              <>
-                {session?.user?.username}{' '}
-                <Icons.ChevronDown className='ml-2 h-4 w-4' />
-              </>
-            )}
-          </Dropdown.Title>
-          <Dropdown.Items className='rounded-md border border-mauve-600 bg-mauve-200 shadow-lg dark:border-mauveDark-600 dark:bg-mauveDark-200'>
-            {links.map(({ href, label, icon, isExternal }) => (
-              <Dropdown.Item key={href}>
-                {isExternal ? (
-                  <a
-                    className={menuItemStyles}
-                    href={href}
-                    rel='noopener noreferrer'
-                    target='_blank'
-                  >
-                    {icon}
-                    {label}
-                  </a>
-                ) : (
-                  <Link className={menuItemStyles} href={href}>
-                    {icon}
-                    {label}
-                  </Link>
-                )}
-              </Dropdown.Item>
-            ))}
-
-            <Dropdown.Item>
-              <button
-                className={menuItemStyles}
-                onClick={() => signOut({ callbackUrl: '/' })}
-              >
-                <Icons.LogOut className='mr-2 h-4 w-4' />
-                Sign out
-              </button>
-            </Dropdown.Item>
-          </Dropdown.Items>
-        </Dropdown>
+        <AuthDropdown
+          isLoading={status === 'loading' ? true : false}
+          username={session?.user?.username || ''}
+        />
       )}
     </header>
   );
