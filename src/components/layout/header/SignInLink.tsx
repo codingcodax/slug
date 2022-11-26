f4e9b04e4723083512679b5715444f79fbba5cd0
Alexis Guzman <alexis12989x@gmail.com>
2022-11-26 16:10:15 -0600

    feat(header): setup sign in link

--- src/components/layout/header/SignInLink.tsx
+++ src/components/layout/header/SignInLink.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 const SignInLink = () => {
   return (
-    <div>
-      <p>Sign In Link Component</p>
-    </div>
+    <Link
+      className='outline-focus-visible rounded-md px-3 py-2 transition-colors duration-200 hover:bg-mauve-400 focus-visible:bg-mauve-400 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400'
+      href='/sign-in'
+    >
+      Sign in &rarr;
+    </Link>
   );
 };
 
