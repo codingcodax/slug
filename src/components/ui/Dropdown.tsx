9fda0364ea471fd6249d8504c8529b9269cd1523
Alexis Guzman <alexis12989x@gmail.com>
2022-11-26 15:59:19 -0600

    feat(ui): add styles to dropdown

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,29 +1,55 @@
 import { Menu } from '@headlessui/react';
 
+import cn from '~/utils/cn';
+
 interface DropdownProps {
   children: React.ReactNode;
 }
 
 const Dropdown = ({ children }: DropdownProps) => {
-  return <Menu>{children}</Menu>;
+  return (
+    <Menu as='div' className='relative inline-block'>
+      {children}
+    </Menu>
+  );
 };
 
 interface DropdownTitleProps {
+  className?: string;
   children: React.ReactNode;
 }
 
 // eslint-disable-next-line react/display-name
-Dropdown.Title = ({ children }: DropdownTitleProps) => {
-  return <Menu.Button>{children}</Menu.Button>;
+Dropdown.Title = ({ className, children }: DropdownTitleProps) => {
+  return (
+    <Menu.Button
+      className={cn(
+        'outline-visible-focus transition-colors duration-200',
+        className
+      )}
+    >
+      {children}
+    </Menu.Button>
+  );
 };
 
 interface DropdownItemsProps {
+  className?: string;
   children: React.ReactNode;
 }
 
 // eslint-disable-next-line react/display-name
-Dropdown.Items = ({ children }: DropdownItemsProps) => {
-  return <Menu.Items>{children}</Menu.Items>;
+Dropdown.Items = ({ className, children }: DropdownItemsProps) => {
+  return (
+    <Menu.Items
+      className={cn(
+        'absolute right-0 z-40 mt-2 w-56 origin-top-right p-1 focus:outline-none',
+        className
+      )}
+    >
+      {children}
+    </Menu.Items>
+  );
 };
 
 interface DropdownItemProps {
