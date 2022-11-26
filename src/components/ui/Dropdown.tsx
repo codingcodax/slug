9b666040e689b50a7f2ac296f57a3948d0b6df4c
Alexis Guzman <alexis12989x@gmail.com>
2022-11-26 15:44:40 -0600

    feat(ui): setup dropdown

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,9 +1,37 @@
-const Dropdown = () => {
-  return (
-    <div>
-      <p>Dropdown Component</p>
-    </div>
-  );
+import { Menu } from '@headlessui/react';
+
+interface DropdownProps {
+  children: React.ReactNode;
+}
+
+const Dropdown = ({ children }: DropdownProps) => {
+  return <Menu>{children}</Menu>;
 };
 
+interface DropdownTitleProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+Dropdown.Title = ({ children }: DropdownTitleProps) => {
+  return <Menu.Button>{children}</Menu.Button>;
+};
+
+interface DropdownItemsProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+Dropdown.Items = ({ children }: DropdownItemsProps) => {
+  return <Menu.Items>{children}</Menu.Items>;
+};
+
+interface DropdownItemProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+Dropdown.Item = ({ children }: DropdownItemProps) => {
+  return <Menu.Item>{children}</Menu.Item>;
+};
 export default Dropdown;
