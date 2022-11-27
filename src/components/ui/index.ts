56321ca1b016bf84eace25cb225b53392db6a2dd
Alexis Guzman <alexis12989x@gmail.com>
2022-11-27 09:34:54 -0600

    feat(ui): export external link

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Dropdown } from './Dropdown';
+export { default as ExternalLink } from './ExternalLink';
 export { default as Icons } from './Icons';
 export { default as Skeleton } from './Skeleton';
