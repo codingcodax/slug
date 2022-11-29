58bc7dc05a644f1e45ddc7eeb7bf06ab953b0c63
Alexis Guzman <alexis12989x@gmail.com>
2022-11-29 11:14:32 -0600

    feat(dashboard): setup options dropdown component

--- src/components/pages/dashboard/OptionsDropdown.tsx
+++ src/components/pages/dashboard/OptionsDropdown.tsx
@@ -1,8 +1,58 @@
-const OptionsDropdown = () => {
+import { Dropdown, Icons } from '~/components/ui';
+import type { LinkSchema } from '~/server/trpc/router/linkRouter';
+import getBaseUrl from '~/utils/getBaseUrl';
+
+interface OptionsDropdownProps {
+  slug: LinkSchema['slug'];
+  handleModal: (modal: 'edit' | 'delete') => void;
+}
+
+const OptionsDropdown = ({ slug, handleModal }: OptionsDropdownProps) => {
+  const copytoclipboard = async (text: string) => {
+    try {
+      const clipboardItem = new ClipboardItem({
+        'text/plain': new Blob([text], { type: 'text/plain' }),
+      });
+
+      await navigator.clipboard.write([clipboardItem]);
+    } catch (error) {
+      await navigator.clipboard.writeText(text);
+    }
+  };
+
   return (
-    <div>
-      <p>Options Dropdown</p>
-    </div>
+    <Dropdown>
+      <Dropdown.Title>
+        <Icons.MoreVertical className='h-4 w-4' />
+      </Dropdown.Title>
+
+      <Dropdown.Items>
+        <Dropdown.Item>
+          <button
+            className='dropdown-item'
+            onClick={() => copytoclipboard(`${getBaseUrl()}/${slug}`)}
+          >
+            <Icons.Copy className='mr-2 h-4 w-4' />
+            Copy
+          </button>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <button className='dropdown-item' onClick={() => handleModal('edit')}>
+            Edit
+          </button>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <button
+            className='dropdown-item'
+            onClick={() => handleModal('delete')}
+          >
+            Delete
+          </button>
+        </Dropdown.Item>
+      </Dropdown.Items>
+    </Dropdown>
   );
 };
 
