14cf8c85847b8c812352e80735df16b33039fd48
Alexis Guzman <alexis12989x@gmail.com>
2022-11-29 09:08:01 -0600

    feat(ui): export modal

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Dropdown } from './Dropdown';
 export { default as ExternalLink } from './ExternalLink';
 export { default as Icons } from './Icons';
+export { default as Modal } from './Modal';
 export { default as Skeleton } from './Skeleton';
