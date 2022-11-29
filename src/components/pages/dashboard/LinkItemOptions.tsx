eec6785477253c57d59d79707f320b6b6fb605e7
Alexis Guzman <alexis12989x@gmail.com>
2022-11-29 11:15:17 -0600

    feat(dashboard): setup link item options component

--- src/components/pages/dashboard/LinkItemOptions.tsx
+++ src/components/pages/dashboard/LinkItemOptions.tsx
@@ -1,7 +1,38 @@
-const LinkItemOptions = () => {
+import { useState } from 'react';
+
+import type { LinkSchema } from '~/server/trpc/router/linkRouter';
+
+import OptionsDropdown from './OptionsDropdown';
+
+export interface LinkItemOptionsProps {
+  id: LinkSchema['id'];
+  slug: LinkSchema['slug'];
+  url: LinkSchema['url'];
+  description: LinkSchema['description'];
+}
+
+const LinkItemOptions = ({
+  id,
+  slug,
+  url,
+  description,
+}: LinkItemOptionsProps) => {
+  const [modalsState, setModalsState] = useState({
+    editModal: false,
+    deleteModal: false,
+  });
+
+  const handleModal = (modal: 'edit' | 'delete') => {
+    if (modal === 'edit')
+      setModalsState((prev) => ({ ...prev, editModal: !prev.editModal }));
+
+    if (modal === 'delete')
+      setModalsState((prev) => ({ ...prev, deleteModal: !prev.deleteModal }));
+  };
+
   return (
-    <div>
-      <p>Link Item Options Component</p>
+    <div className='absolute top-4 right-4'>
+      <OptionsDropdown handleModal={handleModal} slug={slug} />
     </div>
   );
 };
