3c4ea1373540bb21c050470bd00af1beaf35efc5
Alexis Guzman <alexis12989x@gmail.com>
2022-11-29 13:50:20 -0600

    feat(types): create edit link schema

--- src/types/link.d.ts
+++ src/types/link.d.ts
@@ -3,7 +3,9 @@ import type { z } from 'zod';
 import type {
   LinkSchema,
   CreateLinkSchema,
+  EditLinkSchema,
 } from '~/server/trpc/router/linkRouter';
 
 export type LinkSchema = z.infer<typeof LinkSchema>;
 export type CreateLinkSchema = z.infer<typeof CreateLinkSchema>;
+export type EditLinkSchema = z.infer<typeof EditLinkSchema>;
