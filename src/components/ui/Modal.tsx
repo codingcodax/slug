3b44f21ea1579126e8892aeea9b7c644dc18da6b
Alexis Guzman <alexis12989x@gmail.com>
2022-11-29 16:23:04 -0600

    feat(ui): add styles to modal title and description

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -34,7 +34,9 @@ const Modal = ({ show, onClose, children }: ModalProps) => {
               leaveFrom='opacity-100 scale-100'
               leaveTo='opacity-0 scale-95'
             >
-              <Dialog.Panel>{children}</Dialog.Panel>
+              <Dialog.Panel className='w-full max-w-xl transform overflow-hidden rounded-md bg-mauve-100 p-6 text-left shadow-xl transition-all dark:bg-mauveDark-100'>
+                {children}
+              </Dialog.Panel>
             </Transition.Child>
           </div>
         </div>
@@ -49,7 +51,9 @@ interface ModalTitleProps {
 
 // eslint-disable-next-line react/display-name
 Modal.Title = ({ children }: ModalTitleProps) => {
-  return <Dialog.Title>{children}</Dialog.Title>;
+  return (
+    <Dialog.Title className='text-3xl font-medium'>{children}</Dialog.Title>
+  );
 };
 
 interface ModalDescriptionProps {
@@ -58,7 +62,11 @@ interface ModalDescriptionProps {
 
 // eslint-disable-next-line react/display-name
 Modal.Description = ({ children }: ModalDescriptionProps) => {
-  return <Dialog.Description>{children}</Dialog.Description>;
+  return (
+    <Dialog.Description className='mb-10 text-mauve-1100 dark:text-mauveDark-1100'>
+      {children}
+    </Dialog.Description>
+  );
 };
 
 interface ModalBodyProps {
