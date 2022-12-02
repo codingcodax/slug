b915d400a07c9e0ac4b60b2e4a957d84d5466e9c
Alexis Guzman <codingcodax@gmail.com>
2022-12-02 17:54:34 -0600

    chore(seo): create additional link tags for default config

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -44,4 +44,31 @@ export const additionalMetaTags = [
   },
 ];
 
-export const additionalLinkTags = [];
+export const additionalLinkTags = [
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/favicons/apple-touch-icon.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicons/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicons/favicon-16x16.png',
+  },
+  {
+    rel: 'manifest',
+    href: '/favicons/site.manifest',
+  },
+  {
+    rel: 'mask-icon',
+    href: '/favicons/safari-pinned-tab.svg',
+    color: '#0e0e11',
+  },
+];
