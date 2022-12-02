d3c1572628895f1fd34f17629bc970897ceea4b0
Alexis Guzman <codingcodax@gmail.com>
2022-12-02 17:44:50 -0600

    feat(footer): add hover styles to links

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
       <p className='py-4 text-center'>
         Handcrafted with <span className='font-mono'>🤍</span> and{' '}
         <ExternalLink
-          className='font-medium underline decoration-wavy underline-offset-2'
+          className='font-medium underline decoration-wavy underline-offset-2 transition-colors duration-200 hover:text-mauve-1200/80 dark:hover:text-mauveDark-1200/80'
           href='https://create.t3.gg/'
         >
           t3-stack
         </ExternalLink>{' '}
         by{' '}
         <ExternalLink
-          className='font-medium underline decoration-wavy underline-offset-2'
+          className='font-medium underline decoration-wavy underline-offset-2  transition-colors duration-200 hover:text-mauve-1200/80 dark:hover:text-mauveDark-1200/80'
           href='https://www.codingcodax.dev/'
         >
           &#64;codingcodax
