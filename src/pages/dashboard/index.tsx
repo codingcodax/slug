650b6f8882d0289e809c7d1d7230c8e72f822bf6
Alexis Guzman <codingcodax@gmail.com>
2022-12-03 12:42:20 -0600

    feat(dashboard): use create modal instead of new page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,13 @@
 import type { ChangeEvent } from 'react';
 import { useState } from 'react';
 import { type GetServerSideProps } from 'next';
-import Link from 'next/link';
 
 import { trpc } from '~/utils/trpc';
 import { getServerAuthSession } from '~/server/common/get-server-auth-session';
 import { Seo } from '~/components';
 import { Icons } from '~/components/ui';
 import { AnyLinks, Links, Searchbar } from '~/components/pages/dashboard';
+import { CreateModal } from '~/components/modals';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
@@ -23,6 +23,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const Dashboard = () => {
   const { data: links, isLoading } = trpc.link.getAll.useQuery();
   const [search, setSearch] = useState('');
+  const [isNewModalOpen, setIsNewModalOpen] = useState(false);
 
   const filteredLinks = links?.filter((link) =>
     link.slug.toLowerCase().includes(search.toLowerCase())
@@ -32,6 +33,8 @@ const Dashboard = () => {
     setSearch(e.target.value);
   };
 
+  const handleModal = () => setIsNewModalOpen((prev) => !prev);
+
   return (
     <>
       <Seo name='Dashboard' path='/dashboard' />
@@ -39,12 +42,12 @@ const Dashboard = () => {
       <main className='mx-auto w-full max-w-6xl'>
         <section className='mb-10 flex items-center justify-between'>
           <h1 className='text-3xl font-medium'>Dashboard</h1>
-          <Link
+          <button
             className='outline-focus-visible flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 focus-visible:bg-mauve-400 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400'
-            href='/dashboard/new'
+            onClick={handleModal}
           >
             <Icons.Plus className='mr-2 h-4 w-4' /> Create new link
-          </Link>
+          </button>
         </section>
 
         <Searchbar
@@ -56,6 +59,8 @@ const Dashboard = () => {
         {isLoading && <Links.Skeleton />}
         {links && <Links links={filteredLinks} />}
         {links?.length === 0 && <AnyLinks />}
+
+        <CreateModal show={isNewModalOpen} onClose={handleModal} />
       </main>
     </>
   );
