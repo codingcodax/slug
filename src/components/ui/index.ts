1d2a4aca5a471932e86348ae689bd90390bee87c
Alexis Guzman <codingcodax@gmail.com>
2022-12-03 11:06:29 -0600

    feat(ui): export form

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export { default as Dropdown } from './Dropdown';
 export { default as ExternalLink } from './ExternalLink';
+export { default as Form } from './Form';
 export { default as Icons } from './Icons';
 export { default as Modal } from './Modal';
 export { default as Skeleton } from './Skeleton';
