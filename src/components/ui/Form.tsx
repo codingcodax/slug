b0336cf08e3331cee252e5a94f29e5f5aa0130fe
Alexis Guzman <codingcodax@gmail.com>
2022-12-03 11:06:22 -0600

    feat(ui): setup form

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -1,9 +1,41 @@
-const Form = () => {
+interface FormProps {
+  onSubmit?: React.DOMAttributes<HTMLFormElement>['onSubmit'];
+  children: React.ReactNode;
+}
+
+const Form = ({ onSubmit, children }: FormProps) => {
   return (
-    <div>
-      <p>Form Component</p>
-    </div>
+    <form className='grid gap-y-8' onSubmit={onSubmit}>
+      {children}
+    </form>
   );
+};
+
+interface FormItemsProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+Form.Items = ({ children }: FormItemsProps) => {
+  return <div className='space-y-4'>{children}</div>;
+};
+
+interface FormItemProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+Form.Item = ({ children }: FormItemProps) => {
+  return <div className='grid grid-flow-row gap-y-1'>{children}</div>;
+};
+
+interface FormButtonsProps {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+Form.Buttons = ({ children }: FormButtonsProps) => {
+  return <div className='grid grid-cols-2 gap-x-4'>{children}</div>;
 };
 
 export default Form;
