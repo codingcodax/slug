6f0ab84fa87345e22c1569bb9c0c92044fda9af8
Alexis Guzman <codingcodax@gmail.com>
2022-12-02 19:55:32 -0600

    feat(header): create logo

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 
+import { Icons } from '~/components/ui';
+
 const Logo = () => {
   return (
-    <Link className='text-2xl' href='/'>
+    <Link className='flex items-center text-2xl' href='/'>
+      <div className='mr-2 flex h-6 w-6 items-center justify-center rounded-md bg-mauve-1200 dark:bg-mauveDark-1200'>
+        <Icons.Link className='h-4 w-4 stroke-mauve-100 dark:stroke-mauveDark-100' />
+      </div>
       slug
     </Link>
   );
