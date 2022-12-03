1a23b1a58e3cc9e8bbab8836cab4fbba3724ec68
Alexis Guzman <codingcodax@gmail.com>
2022-12-02 20:21:29 -0600

    fix(dasbhoard): update links import

--- src/components/pages/dashboard/index.ts
+++ src/components/pages/dashboard/index.ts
@@ -1,3 +1,3 @@
 export { default as AnyLinks } from './AnyLinks';
-export { default as Links } from './Links';
+export { default as Links } from './links';
 export { default as Searchbar } from './Searchbar';
