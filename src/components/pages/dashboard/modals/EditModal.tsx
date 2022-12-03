4e8e1e5368238ca51fa700b818aad00dedcd3dd9
Alexis Guzman <codingcodax@gmail.com>
2022-12-03 07:54:27 -0600

    feat(dashboard): remove div on edit modal

--- src/components/pages/dashboard/modals/EditModal.tsx
+++ src/components/pages/dashboard/modals/EditModal.tsx
@@ -52,93 +52,89 @@ const EditModal = ({
       <Modal.Title>Edit: {slug}</Modal.Title>
       <Modal.Description>Update the URL or the description</Modal.Description>
       <Modal.Body>
-        <div>
-          <form
-            className='grid max-w-xl gap-y-8'
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className='space-y-4'>
-              <div className='grid grid-flow-row gap-y-1'>
-                <label htmlFor='url'>Enter the new URL:</label>
-                <input
-                  className='input'
-                  defaultValue={url}
-                  id='url'
-                  placeholder='https://github.com/codingcodax/'
-                  type='text'
-                  {...register('url', {
-                    required: { value: true, message: 'Please enter a URL' },
-                    minLength: {
-                      value: 10,
-                      message:
-                        'Please enter a valid URL. It should be at least 10 characters long',
-                    },
-                    pattern: {
-                      value: /^https?:\/\//i,
-                      message:
-                        'Please enter a valid URL. It should start with http:// or https://',
-                    },
-                  })}
-                />
+        <form
+          className='grid max-w-xl gap-y-8'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className='space-y-4'>
+            <div className='grid grid-flow-row gap-y-1'>
+              <label htmlFor='url'>Enter the new URL:</label>
+              <input
+                className='input'
+                defaultValue={url}
+                id='url'
+                placeholder='https://github.com/codingcodax/'
+                type='text'
+                {...register('url', {
+                  required: { value: true, message: 'Please enter a URL' },
+                  minLength: {
+                    value: 10,
+                    message:
+                      'Please enter a valid URL. It should be at least 10 characters long',
+                  },
+                  pattern: {
+                    value: /^https?:\/\//i,
+                    message:
+                      'Please enter a valid URL. It should start with http:// or https://',
+                  },
+                })}
+              />
 
-                {errors.url && <Error>{errors.url.message?.toString()}</Error>}
-              </div>
-
-              <div className='grid grid-flow-row gap-y-1'>
-                <label htmlFor='slug'>
-                  Enter your new slug:
-                  <br />
-                  <span className='text-sm text-mauve-1100 dark:text-mauveDark-1100'>
-                    https://slug.codingcodax.dev/&#36;&#10100;your-slug&#10101;
-                  </span>
-                </label>
-                <input
-                  className='input'
-                  defaultValue={slug}
-                  id='slug'
-                  placeholder='my-portfolio'
-                  type='text'
-                  {...register('slug', {
-                    required: {
-                      value: true,
-                      message:
-                        'Please enter a custom slug or generate a random one',
-                    },
-                    pattern: {
-                      value: /^[a-zA-Z0-9_-]+$/i,
-                      message:
-                        'Please enter a valid slug with blank space or special characters.',
-                    },
-                  })}
-                />
-
-                {errors.slug && (
-                  <Error>{errors.slug.message?.toString()}</Error>
-                )}
-              </div>
-
-              <div className='grid grid-flow-row gap-y-1'>
-                <label htmlFor='description'>Description:</label>
-                <textarea
-                  className={cn('input max-h-32')}
-                  defaultValue={description || ''}
-                  id='description'
-                  placeholder='This link redirecs to my awesome portfolio'
-                  {...register('description')}
-                />
-              </div>
+              {errors.url && <Error>{errors.url.message?.toString()}</Error>}
             </div>
 
-            <div className='grid grid-cols-2 gap-x-4'>
-              <button className='primary-button' type='submit'>
-                {isLoading ? 'Updating your link' : 'Update your link'}
-              </button>
-              <button className='secondary-link' onClick={onClose}>
-                Cancel
-              </button>
+            <div className='grid grid-flow-row gap-y-1'>
+              <label htmlFor='slug'>
+                Enter your new slug:
+                <br />
+                <span className='text-sm text-mauve-1100 dark:text-mauveDark-1100'>
+                  https://slug.codingcodax.dev/&#36;&#10100;your-slug&#10101;
+                </span>
+              </label>
+              <input
+                className='input'
+                defaultValue={slug}
+                id='slug'
+                placeholder='my-portfolio'
+                type='text'
+                {...register('slug', {
+                  required: {
+                    value: true,
+                    message:
+                      'Please enter a custom slug or generate a random one',
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9_-]+$/i,
+                    message:
+                      'Please enter a valid slug with blank space or special characters.',
+                  },
+                })}
+              />
+
+              {errors.slug && <Error>{errors.slug.message?.toString()}</Error>}
             </div>
-          </form>
-        </div>
+
+            <div className='grid grid-flow-row gap-y-1'>
+              <label htmlFor='description'>Description:</label>
+              <textarea
+                className={cn('input max-h-32')}
+                defaultValue={description || ''}
+                id='description'
+                placeholder='This link redirecs to my awesome portfolio'
+                {...register('description')}
+              />
+            </div>
+          </div>
+
+          <div className='grid grid-cols-2 gap-x-4'>
+            <button className='primary-button' type='submit'>
+              {isLoading ? 'Updating your link' : 'Update your link'}
+            </button>
+            <button className='secondary-link' onClick={onClose}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </Modal.Body>
     </Modal>
   );
