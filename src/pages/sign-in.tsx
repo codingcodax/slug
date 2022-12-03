705d00c0adab04419d934ffd53c490e3922f5531
Alexis Guzman <codingcodax@gmail.com>
2022-12-02 19:43:59 -0600

    feat(signin): add seo

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,6 +1,7 @@
 import { type GetServerSideProps } from 'next';
 import { signIn } from 'next-auth/react';
 
+import { Seo } from '~/components';
 import { Icons } from '~/components/ui';
 import { getServerAuthSession } from '~/server/common/get-server-auth-session';
 
@@ -17,21 +18,25 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 const SignIn = () => {
   return (
-    <main className='mx-auto mt-10 flex max-w-6xl flex-col items-center space-y-10'>
-      <div className='space-y-2 text-center'>
-        <h1 className='text-3xl font-medium'>Sign in to slug</h1>
-        <p className='text-mauve-1100'>
-          Short and manage your links like a pro.
-        </p>
-      </div>
-      <button
-        className='flex items-center rounded-md bg-mauve-400 px-4 py-2 text-mauve-100 transition-colors duration-200 dark:bg-mauveDark-400 dark:text-mauveDark-1200 hover:dark:bg-mauveDark-500'
-        onClick={() => signIn('github', { callbackUrl: '/dashboard' })}
-      >
-        <Icons.Github className='mr-2 h-4 w-4' />
-        Sign in with GitHub
-      </button>
-    </main>
+    <>
+      <Seo name='Sign In' path='/sign-in' />
+
+      <main className='mx-auto mt-10 flex max-w-6xl flex-col items-center space-y-10'>
+        <div className='space-y-2 text-center'>
+          <h1 className='text-3xl font-medium'>Sign in to slug</h1>
+          <p className='text-mauve-1100'>
+            Short and manage your links like a pro.
+          </p>
+        </div>
+        <button
+          className='flex items-center rounded-md bg-mauve-400 px-4 py-2 text-mauve-100 transition-colors duration-200 dark:bg-mauveDark-400 dark:text-mauveDark-1200 hover:dark:bg-mauveDark-500'
+          onClick={() => signIn('github', { callbackUrl: '/dashboard' })}
+        >
+          <Icons.Github className='mr-2 h-4 w-4' />
+          Sign in with GitHub
+        </button>
+      </main>
+    </>
   );
 };
 
