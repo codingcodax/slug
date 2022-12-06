3089b60bf7dbb83beb4a69ae7a052ec0da291e52
Alexis Guzman <codingcodax@gmail.com>
2022-12-06 12:03:05 -0600

    feat(header): add outline focus visible class to logo

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -4,7 +4,10 @@ import { Icons } from '~/components/ui';
 
 const Logo = () => {
   return (
-    <Link className='flex items-center text-2xl' href='/'>
+    <Link
+      className='outline-focus-visible flex items-center rounded-md text-2xl'
+      href='/'
+    >
       <div className='mr-2 flex h-6 w-6 items-center justify-center rounded-md bg-mauve-1200 dark:bg-mauveDark-1200'>
         <Icons.Link className='h-4 w-4 stroke-mauve-100 dark:stroke-mauveDark-100' />
       </div>
