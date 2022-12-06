bb15351fe0ec8feb739aae726d6ccf7b812efbc0
Alexis Guzman <codingcodax@gmail.com>
2022-12-06 12:37:07 -0600

    feat(links): remove margin right style to copy button

--- src/components/pages/dashboard/links/CopyButton.tsx
+++ src/components/pages/dashboard/links/CopyButton.tsx
@@ -21,7 +21,7 @@ const CopyButton = ({ slug }: CopyButtonProps) => {
 
   return (
     <button
-      className='outline-focus-visible hover:jtransition-colors ml-2 rounded-md p-1 hover:bg-mauve-400 hover:duration-200 focus-visible:bg-mauve-400 active:bg-mauve-500 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400 dark:active:bg-mauveDark-500'
+      className='outline-focus-visible hover:jtransition-colors rounded-md p-1 hover:bg-mauve-400 hover:duration-200 focus-visible:bg-mauve-400 active:bg-mauve-500 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400 dark:active:bg-mauveDark-500'
       type='button'
       onClick={handleCopy}
     >
