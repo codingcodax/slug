584b2d8a100091c132428b067746de724a38b0e7
Alexis Guzman <codingcodax@gmail.com>
2022-12-06 12:35:57 -0600

    feat(links): add clicks prop to slug in link item

--- src/components/pages/dashboard/links/index.tsx
+++ src/components/pages/dashboard/links/index.tsx
@@ -9,9 +9,11 @@ interface LinksProps {
 const Links = ({ links }: LinksProps) => {
   return (
     <ul className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-      {links?.map(({ id, slug, url, description }) => (
+      {links?.map(({ id, slug, url, description, clicks }) => (
         <LinkItem key={id}>
-          <LinkItem.Slug slug={slug}>{slug}</LinkItem.Slug>
+          <LinkItem.Slug clicks={clicks} slug={slug}>
+            {slug}
+          </LinkItem.Slug>
           <LinkItem.Link>{url}</LinkItem.Link>
           <LinkItem.Description>{description}</LinkItem.Description>
           <LinkItem.Options
