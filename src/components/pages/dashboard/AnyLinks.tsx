58e07d1f204afa0992b466ad37c2d5b7bb193fa6
Alexis Guzman <codingcodax@gmail.com>
2022-12-06 10:47:55 -0600

    feat(dashboard): add outline focus visible styles to any links component

--- src/components/pages/dashboard/AnyLinks.tsx
+++ src/components/pages/dashboard/AnyLinks.tsx
@@ -7,7 +7,7 @@ const AnyLinks = () => {
     <section className='flex flex-col items-center'>
       <p>You don&apos;t have any links.</p>
       <Link
-        className='flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 dark:hover:bg-mauveDark-400'
+        className='outline-focus-visible mt-2 flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 hover:transition-colors hover:duration-200 focus-visible:bg-mauve-400 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400'
         href='/dashboard/new'
       >
         <Icons.Plus className='mr-2 h-4 w-4' /> Create one first
