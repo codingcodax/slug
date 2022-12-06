789b9ab43db16ec17ccd1b52e2574ff69eef4320
Alexis Guzman <codingcodax@gmail.com>
2022-12-06 10:57:29 -0600

    feat(dashboard): update disables styles for searchbar

--- src/components/pages/dashboard/Searchbar.tsx
+++ src/components/pages/dashboard/Searchbar.tsx
@@ -11,14 +11,21 @@ interface SearchbarProps {
 
 const Searchbar = ({ value, onChange, isDisabled }: SearchbarProps) => {
   return (
-    <div className='relative mb-4 w-full'>
+    <div
+      className={cn(
+        'relative mb-4 w-full',
+        isDisabled ? 'cursor-not-allowed opacity-25' : ''
+      )}
+    >
       <div className='absolute top-0 left-0 flex h-[42px] w-10 items-center justify-center'>
         <Icons.Search className='h-6 w-6 stroke-mauve-1100 dark:stroke-mauveDark-1000' />
       </div>
       <input
         className={cn(
           'input w-full pl-10',
-          isDisabled ? 'cursor-not-allowed' : ''
+          isDisabled
+            ? 'cursor-not-allowed hover:border-mauve-700 dark:hover:border-mauveDark-700'
+            : ''
         )}
         disabled={isDisabled}
         placeholder='Search'
