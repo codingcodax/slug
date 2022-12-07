5305ae90ed7a80278dde5fcc1800e33de03460ea
Alexis Guzman <codingcodax@gmail.com>
2022-12-07 17:52:54 -0600

    feat(types): create type for delete link schema

--- src/types/link.d.ts
+++ src/types/link.d.ts
@@ -4,8 +4,10 @@ import type {
   LinkSchema,
   CreateLinkSchema,
   EditLinkSchema,
+  DeleteLinkSchema,
 } from '~/server/trpc/router/linkRouter';
 
 export type LinkSchema = z.infer<typeof LinkSchema>;
 export type CreateLinkSchema = z.infer<typeof CreateLinkSchema>;
 export type EditLinkSchema = z.infer<typeof EditLinkSchema>;
+export type DeleteLinkSchema = z.infer<typeof DeleteLinkSchema>;
