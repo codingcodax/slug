8f41825d756d7056e7b9ee8382f2b815e74d2976
Alexis Guzman <codingcodax@gmail.com>
2022-12-07 17:53:04 -0600

    feat(forms): setup delete form

--- src/components/forms/DeleteForm.tsx
+++ src/components/forms/DeleteForm.tsx
@@ -1,8 +1,67 @@
-const DeleteForm = () => {
+import type { UseFormRegister } from 'react-hook-form';
+
+import type { LinkSchema } from '~/types/link';
+import { Form } from '~/components/ui';
+import cn from '~/utils/cn';
+
+import Error from './Error';
+
+interface DeleteFormProps {
+  slug: LinkSchema['slug'];
+  idError: string;
+  isDisabled: boolean;
+  register: UseFormRegister<{ id: string }>;
+  isLoading: boolean;
+  onSubmit?: React.DOMAttributes<HTMLFormElement>['onSubmit'];
+  onClose: () => void;
+}
+
+const DeleteForm = ({
+  slug,
+  idError,
+  isDisabled,
+  register,
+  isLoading,
+  onSubmit,
+  onClose,
+}: DeleteFormProps) => {
   return (
-    <div>
-      <p>Delte Form</p>
-    </div>
+    <Form onSubmit={onSubmit}>
+      <Form.Items>
+        <Form.Item>
+          <label htmlFor='id'>
+            Please type <strong>{`/${slug}`}</strong> to confirm
+          </label>
+          <input
+            className='input'
+            id='id'
+            placeholder='...'
+            type='text'
+            {...register('id', { validate: (value) => value === `/${slug}` })}
+          />
+
+          {idError && <Error>{idError}</Error>}
+        </Form.Item>
+      </Form.Items>
+
+      <Form.Buttons>
+        <button
+          className={cn(
+            'primary-button',
+            isDisabled
+              ? 'cursor-not-allowed opacity-25 hover:bg-mauve-1200 dark:hover:bg-mauveDark-1200'
+              : ''
+          )}
+          disabled={isDisabled}
+          type='submit'
+        >
+          {isLoading ? 'Deleting your link' : 'Delete your link'}
+        </button>
+        <button className='secondary-button' type='button' onClick={onClose}>
+          Cancel
+        </button>
+      </Form.Buttons>
+    </Form>
   );
 };
 
