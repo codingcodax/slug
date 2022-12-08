3aacdd17d29aa20bb28484d1a4da1b6c1d1b900a
Alexis Guzman <codingcodax@gmail.com>
2022-12-08 16:23:22 -0600

    feat(ui): add user icon

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -14,6 +14,7 @@ import {
   Plus,
   Search,
   Trash2,
+  User,
 } from 'lucide-react';
 
 const Icons = {
@@ -32,6 +33,7 @@ const Icons = {
   Plus,
   Search,
   Trash2,
+  User,
 };
 
 export default Icons;
