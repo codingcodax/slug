8bc388454a0ab671b12aefbba2e69f59b0ece6ce
Alexis Guzman <codingcodax@gmail.com>
2022-12-08 17:03:24 -0600

    feat(profile): setup user component

--- src/components/pages/profile/User.tsx
+++ src/components/pages/profile/User.tsx
@@ -1,8 +1,17 @@
-const User = () => {
+import Image from 'next/image';
+
+interface UserProps {
+  name: string;
+  username: string;
+  imageUrl: string;
+}
+
+const User = ({ name, username, imageUrl }: UserProps) => {
   return (
-    <div>
-      <p>User</p>
-    </div>
+    <section>
+      <Image alt={`${name} profile`} height={100} src={imageUrl} width={100} />
+      <p>&#x40;{username}</p>
+    </section>
   );
 };
 
