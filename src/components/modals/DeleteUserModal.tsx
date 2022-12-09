73e704ab457814cc503d002c8a82e3e5058879b5
Alexis Guzman <codingcodax@gmail.com>
2022-12-09 10:17:52 -0600

    feat(modals): setup delete user

--- src/components/modals/DeleteUserModal.tsx
+++ src/components/modals/DeleteUserModal.tsx
@@ -1,8 +1,69 @@
-const DeleteUserModal = () => {
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { signOut } from 'next-auth/react';
+import type { User } from '@prisma/client';
+
+import type { DeleteUserSchema } from '~/types/user';
+import { trpc } from '~/utils/trpc';
+import { Modal } from '~/components/ui';
+import DeleteUserForm from '~/components/forms/DeleteUserForm';
+
+interface DeleteUserModalProps extends DeleteUserSchema {
+  username: User['username'];
+  show: boolean;
+  onClose: () => void;
+}
+
+const DeleteUserModal = ({
+  id,
+  username,
+  show,
+  onClose,
+}: DeleteUserModalProps) => {
+  const {
+    handleSubmit,
+    register,
+    setError,
+    formState: { errors, isValid },
+  } = useForm<{ id: string }>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { mutate: deleteUser } = trpc.user.delete.useMutation({
+    onSuccess: () => {
+      signOut({ callbackUrl: '/' });
+      onClose();
+      setIsLoading(false);
+    },
+    onError: () => {
+      setIsLoading(false);
+      setError('id', {
+        type: 'manual',
+        message: 'Your account cannot be deleted, please try again later',
+      });
+    },
+  });
+
+  const onSubmit = () => {
+    setIsLoading(true);
+    deleteUser({ id });
+  };
+
   return (
-    <div>
-      <p>Delete User Modal</p>
-    </div>
+    <Modal show={show} onClose={onClose}>
+      <Modal.Title>Delete accout</Modal.Title>
+      <Modal.Description>Delete your account permanently</Modal.Description>
+      <Modal.Body>
+        <DeleteUserForm
+          idError={errors.id?.message || ''}
+          isDisabled={isValid ? false : true}
+          isLoading={isLoading}
+          register={register}
+          username={username}
+          onClose={onClose}
+          onSubmit={handleSubmit(onSubmit)}
+        />
+      </Modal.Body>
+    </Modal>
   );
 };
 
