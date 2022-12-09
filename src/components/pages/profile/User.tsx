bfc5b6f591ed8df2569e5214984e1fc9276cd3c9
Alexis Guzman <codingcodax@gmail.com>
2022-12-09 10:18:14 -0600

    feat(profile): setup user component

--- src/components/pages/profile/User.tsx
+++ src/components/pages/profile/User.tsx
@@ -1,17 +1,23 @@
+import { User } from '@prisma/client';
 import Image from 'next/image';
 
 import { Skeleton } from '~/components/ui';
 
 interface UserProps {
-  name: string;
-  username: string;
-  imageUrl: string;
+  name: User['name'];
+  username: User['username'];
+  imageUrl: User['image'];
 }
 
 const User = ({ name, username, imageUrl }: UserProps) => {
   return (
     <div className='flex flex-col items-center space-y-2 rounded-sm'>
-      <Image alt={`${name} profile`} height={100} src={imageUrl} width={100} />
+      <Image
+        alt={`${name} profile`}
+        height={100}
+        src={imageUrl || ''}
+        width={100}
+      />
       <p>&#x40;{username}</p>
     </div>
   );
