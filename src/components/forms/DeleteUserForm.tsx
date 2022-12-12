47c178f8b1e1d2ca92d38952dea578c5c3aea096
Alexis Guzman <codingcodax@gmail.com>
2022-12-11 21:07:15 -0600

    feat(forms): convert to error button

--- src/components/forms/DeleteUserForm.tsx
+++ src/components/forms/DeleteUserForm.tsx
@@ -49,10 +49,8 @@ const DeleteUserForm = ({
       <Form.Buttons>
         <button
           className={cn(
-            'primary-button',
-            isDisabled
-              ? 'cursor-not-allowed opacity-40 hover:bg-mauve-1200 dark:hover:bg-mauveDark-1200'
-              : ''
+            'error-button',
+            isDisabled ? 'cursor-not-allowed opacity-40 hover:bg-red-900' : ''
           )}
           type='submit'
         >
