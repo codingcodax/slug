16ac003af60babc3efb5725473a023b89bc4dc88
Alexis Guzman <codingcodax@gmail.com>
2022-12-13 16:10:59 -0600

    feat(forms): setup randomize button component

--- src/components/forms/RandomizeButton.tsx
+++ src/components/forms/RandomizeButton.tsx
@@ -1,8 +1,18 @@
-const RandomizeButton = () => {
+import { Icons } from '~/components/ui';
+
+interface RandomizeButtonProps {
+  onClick: () => void;
+}
+
+const RandomizeButton = ({ onClick }: RandomizeButtonProps) => {
   return (
-    <div>
-      <p>Randomize Button Component</p>
-    </div>
+    <button
+      type='button'
+      className='outline-focus-visible mb-1 flex items-center rounded-sm text-sm text-mauve-1100 transition-colors duration-200 hover:text-mauve-1200 dark:text-mauveDark-1100 dark:hover:text-mauveDark-1200'
+      onClick={onClick}
+    >
+      <Icons.Dices className='mr-2 h-4 w-4' /> Randomize
+    </button>
   );
 };
 
