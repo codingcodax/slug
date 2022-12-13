e716ef5029cf01876303b9d285645cad48cb438f
Alexis Guzman <codingcodax@gmail.com>
2022-12-12 19:59:14 -0600

    feat(home): add hero image and update wrapper styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import { type NextPage } from 'next';
+import Image from 'next/image';
 
 import { Hero } from '~/components/pages/home';
 
+import HeroImage from '../../public/_static/images/slug-preview-mockup.png';
+
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='mx-auto w-full max-w-6xl'>
       <Hero />
+
+      <div className='mt-24'>
+        <Image
+          alt='dashboard page preview from slug as mockup'
+          src={HeroImage}
+        />
+      </div>
     </div>
   );
 };
