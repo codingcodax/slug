495a73678637a1d46c84577e1189a8f681b59612
Alexis Guzman <codingcodax@gmail.com>
2022-12-13 20:30:01 -0600

    feat(modals): export account modal

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,3 +1,4 @@
+export { default as AccountModal } from './AccountModal';
 export { default as CreateModal } from './CreateModal';
 export { default as DeleteUserModal } from './DeleteUserModal';
 export { default as EditModal } from './EditModal';
