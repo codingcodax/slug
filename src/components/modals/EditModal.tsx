611b41634967b80fd4d8f2da5256de2a0e7b3acf
Alexis Guzman <codingcodax@gmail.com>
2022-12-14 13:33:02 -0600

    feat(modals): use edit modal atoms

--- src/components/modals/EditModal.tsx
+++ src/components/modals/EditModal.tsx
@@ -1,39 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useAtom } from 'jotai';
 import { useForm } from 'react-hook-form';
 
 import type { EditLinkSchema } from '~/types/link';
+import { editModalIsOpenAtom, editModalDataAtom } from '~/store/modals';
 import { trpc } from '~/utils/trpc';
 import { Modal } from '~/components/ui';
 import { EditForm } from '~/components/forms';
 
-interface EditModalProps extends EditLinkSchema {
-  show: boolean;
-  onClose: () => void;
-}
-
-const EditModal = ({
-  show,
-  onClose,
-  id,
-  slug,
-  url,
-  description,
-}: EditModalProps) => {
+const EditModal = () => {
+  const [isOpen, setIsOpen] = useAtom(editModalIsOpenAtom);
+  const [{ id, slug, url, description }, setData] = useAtom(editModalDataAtom);
+  const [isLoading, setIsLoading] = useState(false);
   const {
     handleSubmit,
     register,
+    reset,
     setError,
     setValue,
     formState: { errors },
   } = useForm<EditLinkSchema>();
-  const [isLoading, setIsLoading] = useState(false);
 
   const { refetch } = trpc.link.getAll.useQuery();
   const { mutate: editLink } = trpc.link.edit.useMutation({
     onSuccess: () => {
       refetch();
-      onClose();
+      setIsOpen(false);
       setIsLoading(false);
+      setData({ id: -1, slug: '', url: '', description: '' });
     },
     onError: () => {
       setIsLoading(false);
@@ -45,18 +39,26 @@ const EditModal = ({
     },
   });
 
+  useEffect(() => {
+    reset();
+  }, [isOpen, reset]);
+
   const onSubmit = (values: EditLinkSchema) => {
     setIsLoading(true);
     editLink({ ...values, id });
   };
 
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Modal show={show} onClose={onClose}>
+    <Modal show={isOpen} onClose={() => setIsOpen(false)}>
       <Modal.Title>Edit: {slug}</Modal.Title>
       <Modal.Description>Update the URL or the description</Modal.Description>
       <Modal.Body>
         <EditForm
-          description={description || ''}
+          description={description || 'No Description'}
           isLoading={isLoading}
           register={register}
           setValue={setValue}
