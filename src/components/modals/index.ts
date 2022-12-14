4ca4ffe37a0641fc7eb4452dd2c276da6babfe21
Alexis Guzman <codingcodax@gmail.com>
2022-12-14 13:42:40 -0600

    feat(modals): export delete modal

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,4 +1,5 @@
 export { default as AccountModal } from './AccountModal';
 export { default as CreateModal } from './CreateModal';
+export { default as DeleteModal } from './DeleteModal';
 export { default as DeleteUserModal } from './DeleteUserModal';
 export { default as EditModal } from './EditModal';
