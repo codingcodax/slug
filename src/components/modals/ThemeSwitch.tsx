5379499c40ca90475bdea3f72bc83b1a05f857c2
Alexis Guzman <codingcodax@gmail.com>
2022-12-13 20:29:39 -0600

    feat(modals): setup theme switch component

--- src/components/modals/ThemeSwitch.tsx
+++ src/components/modals/ThemeSwitch.tsx
@@ -1,8 +1,55 @@
+import { useTheme } from 'next-themes';
+
+import { Dropdown, Icons } from '~/components/ui';
+import useHasMouted from '~/hooks/useHasMounted';
+
 const ThemeSwitch = () => {
+  const hasMounted = useHasMouted();
+  const { theme, resolvedTheme, setTheme } = useTheme();
+
   return (
-    <div>
-      <p>Theme Switch Component</p>
-    </div>
+    <Dropdown>
+      <Dropdown.Title>
+        Theme{' '}
+        {hasMounted ? (
+          <Icons.Palette className='ml-4 h-4 w-4' />
+        ) : (
+          <Icons.Loader className='ml-4 h-4 w-4 animate-spin' />
+        )}
+      </Dropdown.Title>
+      <Dropdown.Items>
+        <Dropdown.Item>
+          <button
+            className='dropdown-item'
+            type='button'
+            onClick={() => setTheme('light')}
+          >
+            <Icons.Sun className='mr-2 h-4 w-4' />
+            Light
+          </button>
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <button
+            className='dropdown-item'
+            type='button'
+            onClick={() => setTheme('dark')}
+          >
+            <Icons.Moon className='mr-2 h-4 w-4' />
+            Dark
+          </button>
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <button
+            className='dropdown-item'
+            type='button'
+            onClick={() => setTheme('system')}
+          >
+            <Icons.Palette className='mr-2 h-4 w-4' />
+            System
+          </button>
+        </Dropdown.Item>
+      </Dropdown.Items>
+    </Dropdown>
   );
 };
 
