a2e06e0a301408d776825e4661b221caab1dccb9
Alexis Guzman <codingcodax@gmail.com>
2022-12-13 20:47:08 -0600

    feat(dashboard): remove create modal and update atom import

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,11 +5,10 @@ import { useAtom } from 'jotai';
 
 import { trpc } from '~/utils/trpc';
 import { getServerAuthSession } from '~/server/common/get-server-auth-session';
-import { isOpenAtom } from '~/store/createModal';
+import { createModalIsOpenAtom } from '~/store/createModal';
 import { Seo } from '~/components';
 import { Icons } from '~/components/ui';
 import { AnyLinks, Links, Searchbar } from '~/components/pages/dashboard';
-import { CreateModal } from '~/components/modals';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
@@ -25,7 +24,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const Dashboard = () => {
   const { data: links, isLoading } = trpc.link.getAll.useQuery();
   const [search, setSearch] = useState('');
-  const [isOpen, setIsOpen] = useAtom(isOpenAtom);
+  const [, setModalIsOpen] = useAtom(createModalIsOpenAtom);
 
   const filteredLinks = useMemo(() => {
     return links?.filter(
@@ -39,8 +38,6 @@ const Dashboard = () => {
     setSearch(e.target.value);
   };
 
-  const handleModal = () => setIsOpen((prev) => !prev);
-
   return (
     <>
       <Seo name='Dashboard' path='/dashboard' />
@@ -50,7 +47,7 @@ const Dashboard = () => {
           <h1 className='text-3xl font-medium'>Dashboard</h1>
           <button
             className='outline-focus-visible flex items-center rounded-md px-4 py-2 hover:bg-mauve-400 focus-visible:bg-mauve-400 dark:hover:bg-mauveDark-400 dark:focus-visible:bg-mauveDark-400'
-            onClick={handleModal}
+            onClick={() => setModalIsOpen(true)}
           >
             <Icons.Plus className='mr-2 h-4 w-4' /> Create new link
           </button>
@@ -65,8 +62,6 @@ const Dashboard = () => {
         {isLoading && <Links.Skeleton />}
         {links && <Links links={filteredLinks} />}
         {links?.length === 0 && <AnyLinks />}
-
-        <CreateModal show={isOpen} onClose={handleModal} />
       </main>
     </>
   );
